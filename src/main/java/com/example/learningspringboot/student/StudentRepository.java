04ea2e08131cb217d6ddddf70b9c5b42348f940c
akshanshgusain <akshanshgusain@gmail.com>
2022-07-29 17:58:52 +0530

    feat: added post mapping

--- src/main/java/com/example/learningspringboot/student/StudentRepository.java
+++ src/main/java/com/example/learningspringboot/student/StudentRepository.java
@@ -1,8 +1,16 @@
 package com.example.learningspringboot.student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    //Optional<Student> findStudentByEmail(String email);
+    //Or more explicit form:
+    @Query("SELECT s FROM Student s WHERE s.email=?1")
+    Optional<Student> findStudentByEmail(String email);
 
 }
