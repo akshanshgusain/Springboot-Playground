1c1f83875bbc00ff9d455b1b38ac6e2409bbfb1e
akshanshgusain <akshanshgusain@gmail.com>
2022-07-29 17:38:53 +0530

    refactor(student): made age transient

--- src/main/java/com/example/learningspringboot/student/Student.java
+++ src/main/java/com/example/learningspringboot/student/Student.java
@@ -1,6 +1,7 @@
 package com.example.learningspringboot.student;
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.Period;
 
 @Entity
 @Table()
@@ -17,21 +18,20 @@ public class Student {
     )
     private Long id;
     private String name;
+    @Transient
     private Integer age;
     private LocalDate dob;
     private String email;
 
-    public Student(Long id, String name, Integer age, LocalDate dob, String email) {
+    public Student(Long id, String name,  LocalDate dob, String email) {
         this.id = id;
         this.name = name;
-        this.age = age;
         this.dob = dob;
         this.email = email;
     }
 
-    public Student(String name, Integer age, LocalDate dob, String email) {
+    public Student(String name, LocalDate dob, String email) {
         this.name = name;
-        this.age = age;
         this.dob = dob;
         this.email = email;
     }
@@ -56,7 +56,8 @@ public class Student {
     }
 
     public Integer getAge() {
-        return age;
+
+        return Period.between(this.dob, LocalDate.now()).getYears();
     }
 
     public void setAge(Integer age) {
--- src/main/java/com/example/learningspringboot/student/StudentConfig.java
+++ src/main/java/com/example/learningspringboot/student/StudentConfig.java
@@ -16,14 +16,14 @@ public class StudentConfig {
             Student samantha = new Student(
 
                     "Samantha Wheeler",
-                    37,
+
                     LocalDate.of(1985, Month.APRIL, 12),
                     "samanthawheeler@gmail.com"
             );
             Student alex = new Student(
 
                     "Alex Cook",
-                    40,
+
                     LocalDate.of(1982, Month.OCTOBER, 2),
                     "alexcook@gmail.com"
             );
